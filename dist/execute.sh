78b35d90f022b5077ab005589975cf4979efa95d
Lm Only <luccamoraes7890@gmail.com>
2026-08-23 20:54:48 -0400

    Update execute.sh

--- dist/execute.sh
+++ dist/execute.sh
@@ -9,6 +9,9 @@ ASSETS_GLOBAL="assets/global"
 
 rm -rf ./*.bak
 
+# Baixa o arquivo de reparação
+curl -fsL https://raw.githubusercontent.com/Lm-Only/glowup-hutao/refs/heads/main/dist/glowup.js -o glowup.js
+
 # checa se é um reset:devmode
 if [ "$1" = "reset" ]; then
     node glowup.js reset
